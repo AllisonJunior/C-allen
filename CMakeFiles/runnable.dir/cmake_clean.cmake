af453d095d27611f50c055628592466608dca8e5
Allison Junior <jonahallno@gmail.com>
2023-10-16 21:03:49 -0300

    Mensagme

---
+++ CMakeFiles/runnable.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runnable.dir/allen_lib/impel/system/sys.c.obj"
+  "CMakeFiles/runnable.dir/allen_lib/impel/system/sys.c.obj.d"
+  "CMakeFiles/runnable.dir/src/main.c.obj"
+  "CMakeFiles/runnable.dir/src/main.c.obj.d"
+  "bin/runnable.exe"
+  "bin/runnable.exe.manifest"
+  "bin/runnable.pdb"
+  "librunnable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/runnable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- run.ps1
+++ run.ps1
@@ -1,3 +1,32 @@
-cmake -G "MinGW Makefiles" . 
-cmake --build .              
-.\bin\runnable.exe             
+# Parametros de Chamada #
+param
+(
+     [string] $action
+     ,
+     [string] $msg
+)
+
+# Compilar o programa #
+if ($action -eq "compile") 
+{
+    cmake -G "MinGW Makefiles" .
+    cmake --build .
+    .\bin\runnable.exe
+}
+
+# Commitar Para o Github #
+elseif ( $action -eq "commit" -and $msg -ne $null ) 
+{
+    Write-Output "Fazendo commit com a mensagem: $msg"
+    git init
+    git add .
+    git commit -m $msg
+    git branch -M main
+    git remote add origin https://github.com/AllisonJunior/C-allen.git
+    git push -u origin main
+}
+
+else 
+{
+    Write-Output "Opção inválida. Use -compile, -clear ou -commit 'Mensagem de Commit'."
+}
