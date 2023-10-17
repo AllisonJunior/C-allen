f0dc3d9b3a6d4afa3a3eebe87cf583d324ee5919
Allison Junior <jonahallno@gmail.com>
2023-10-16 21:16:43 -0300

    Atualização_no_Arquivo_run.ps1

--- run.ps1
+++ run.ps1
@@ -33,12 +33,19 @@ elseif ( $action -eq "clear" )
 # COMMIT PARA O GITHUB #
 elseif ( $action -eq "commit" -and $msg -ne $null ) 
 {
-      Write-Output "* Limpando Arquivos Desnecessários..."
-
-      Remove-Item -Path "CMakeFiles" -Recurse
-      Remove-Item -Path "cmake_install.cmake"
-      Remove-Item -Path "CMakeCache.txt"
-      Remove-Item -Path "Makefile" 
+      # Verifica se os arquivos existem antes de tentar removê-los
+      if ( Test-Path -Path "CMakeFiles" -or Test-Path -Path "cmake_install.cmake" -or Test-Path -Path "CMakeCache.txt" -or Test-Path -Path "Makefile" ) 
+      {
+        Write-Output "* Os arquivos estão sendo deletados..."
+        Remove-Item -Path "CMakeFiles" -Recurse -Force
+        Remove-Item -Path "cmake_install.cmake" -Force
+        Remove-Item -Path "CMakeCache.txt" -Force
+        Remove-Item -Path "Makefile" -Force
+      }
+      else 
+      {
+        Write-Output "* Os arquivos não existem. Nenhuma ação necessária."
+      }
 
       Write-Output "* Fazendo o commit..."
       git init
