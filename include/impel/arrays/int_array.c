a9759f14424fdc6ca4a5adb04d5a1dacdb58e836
Allison Junior <jonahallno@gmail.com>
2023-10-19 17:54:05 -0300

    Modificações iniciais no header de arrays

---
+++ include/impel/arrays/int_array.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+# include "../../allen/arrays.h"
+
+void initArray ( IntArray * int_array , int size )
+{   
+    int pos = 0;
+    
+    int_array -> size = size;
+    int_array -> start = 0;
+    int_array -> end = 0;    
+    int_array -> list_d = ( int * ) malloc ( sizeof ( int ) * size );
+
+    while ( pos < size )
+    {
+         int_array -> list_d [ pos ] = 0;
+         pos ++;
+    }  
+}
+
+void printArray ( IntArray * int_array )
+{
+    for ( int kaj = 0 ; kaj < int_array -> size ; kaj ++ )
+       printf ( "%d " , int_array -> list_d [ kaj ] );
+}
--- src/main.c
+++ src/main.c
@@ -1,16 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-int array_size ( int * array ) 
-{
-   int element = sizeof ( array [ 0 ] );
-   int total = sizeof ( array [] );
-   return total / element;
-}
-
 int main ( void )
 { 
-   int array [] = { 10 , 20 , 30 , 40 , 50 , 60 , 90 };
-   int tamanho = array_size ( array );
-   printf("O tamanho do array é %d.\n", tamanho);
 }  
