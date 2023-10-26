2f09c855b3ba81a058cff5061668e0552c8732a9
Allen <jonahallno@gmail.com>
2023-10-26 14:37:47 -0300

    Adição da pasta linux

--- run.ps1
+++ run.ps1
@@ -51,6 +51,11 @@ elseif ( $action -eq "commit" -and $msg -ne $null )
 
       Write-Output "* Adicionando arquivos ao commit..."
       git add .
+      git add CMakeLists.txt
+      git add README.MD
+      git add run.ps1
+      git add src/*
+      git add linux/*
 
       Write-Output "* Fazendo o commit..."
       git commit -m $msg
