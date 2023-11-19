57964f78ed5a7a7a08353321aa54bf2d1028a4e3
Allen <jonahallno@gmail.com>
2023-11-19 16:56:51 -0300

    Some changes

---
+++ include/allen/threads.h
@@ -0,0 +1,14 @@
+/*
+LICENSE
+*/
+
+
+# ifndef _THREADS_
+# define _THREADS_
+
+// This is for x64bits basically a uintptr_t -> unsigned long long
+typedef unsigned long long thread;
+
+void init_Thread ( thread * temp );
+
+# endif
--- include/impel/print/generics.c
+++ include/impel/print/generics.c
@@ -8,7 +8,6 @@ LICENSE
 
 # include "../../allen/print.h"
 # include "../../allen/props.h"
-# include "../../allen/simple.h"
 
 /* =============================================================================================== */
 
@@ -16,6 +15,10 @@ LICENSE
 # include <stdarg.h>
 # include <string.h>
 # include <stdlib.h>
+
+/* =============================================================================================== */
+
+static char convert_ascii ( const char check );
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
 
@@ -59,7 +62,7 @@ void putf ( const char * _string , ... )
            switch ( * _string )
            {     
                  //* @i <- int //
-                 case 'i':
+                 case 'i': 
                           //* Check the Parameters
                           init = strchr ( _string , '<' );
                           endt = strchr ( _string , '>' );
@@ -97,7 +100,7 @@ void putf ( const char * _string , ... )
                             else                        printf ( "%0*d" , atoi ( value ) , va_arg ( args , int ) );
                           }
                           else 
-                              printf ( "%d" , va_arg ( args , int ) );     
+                              printf ( "%d" , va_arg ( args , int ) ); 
                  break;
                  
 
@@ -170,12 +173,14 @@ void putf ( const char * _string , ... )
                  
 
                  //* @e <- exponential floating-point number //
-                 case 'e': 
+                 case 'e':
+                          printf ( "%e" , va_arg ( args , double ) );
                  break;
 
 
                  //* @o <- octal number // 
-                 case 'o': 
+                 case 'o':
+                          printf ( "%o" , va_arg ( args , double ) );
                  break; 
 
 
@@ -186,19 +191,19 @@ void putf ( const char * _string , ... )
 
                  //* Random //
                  case '@': printf ( "@" ); break;
-                 default: printf ( "@" );  break;
+                 default:  printf ( "%c" , * _string );  break;
            }
          }
          
          //* ASCII FIX ON WINDOWS 
          # if defined ( _WIN32 ) || defined ( _WIN64 )
 
-         else if ( * _string == -61 )
-         {
-             * _string ++;
-             printf ( "%c" , check_ascii_chars ( * _string ) );
-         }
-          	  
+          else if ( * _string == -61 )
+          {
+              * _string ++;
+              printf ( "%c" , convert_ascii ( * _string ) );
+          }
+
          # endif 
         
           
@@ -213,3 +218,79 @@ void putf ( const char * _string , ... )
 }
 
 
+
+// EXCLUSIVE FUNCTIONS //
+
+
+// CHECK FOR THE ESPECIFIC CHAR AND PRINT // 
+static char convert_ascii ( const char _check )
+{   
+    switch ( _check )
+    {     
+          // 'A' cases //
+          case -93:  return 198; //* ã
+          case -125: return 199; //* Ã
+
+          case -94:  return 131; //* â                   
+          case -126: return 182; //* Â
+
+          case -95:  return 160; //* á 
+          case -127: return 181; //* Á 
+
+          case -96:  return 133; //* à
+          case -128: return 183; //* À
+         
+          
+          // 'Ç' cases //
+          case -89:  return 135; //* ç
+          case -121: return 128; //* Ç
+          
+
+          // 'E' cases // 
+          case -86:  return 136; //* ê  
+          case -118: return 210; //* Ê              
+          
+          case -87:  return 130; //* é 
+          case -119: return 144; //* É
+          
+          case -88:  return 138; //* è 
+          case -120: return 212; //* È                    
+                   
+                   
+          // 'I' cases //                   
+          case -82:  return 140; //* î 
+          case -114: return 215; //* Î               
+          
+          case -83:  return 161; //* í
+          case -115: return 214; //* Í 
+          
+          case -84:  return 141; //* ì 
+          case -116: return 222; //* Ì                    
+                   
+
+          // 'O' cases //
+          case -75:  return 228; //* õ 
+          case -107: return 229; //* Õ
+          
+          case -76:  return 147; //* ô               
+          case -108: return 226; //* Ô 
+          
+          case -77:  return 162; //* ó   
+          case -109: return 224; //* Ó 
+            
+          case -78:  return 149; //* ò 
+          case -110: return 227; //* Ò
+
+          // 'U' cases //
+          case -69:  return 150; //* û
+          case -101: return 234; //* Û            
+          
+          case -70:  return 163; //* ú
+          case -102: return 233; //* Ú          
+          
+          case -71:  return 151; //* ù
+          case -103: return 235; //* Ù
+
+          // Random Chars //          
+    }
+}
